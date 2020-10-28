0597957229657e6b6bfe3f9047dafd31b4612060
vedadovsky <61380074+vedadovsky@users.noreply.github.com>
2020-10-28 14:29:40 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+$('.slider-one').not(".slick-initialized").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    nextArrow: ".site-slider .slider-btn .next",
+    prevArrow: ".site-slider .slider-btn .prev"
+});
